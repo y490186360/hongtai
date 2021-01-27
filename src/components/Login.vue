6f705e49006259ea7f6f87a82ff1f2d5bd328b39
Yuki <490186360@qq.com>
2021-01-27 18:39:03 +0800

    登录框框

---
+++ src/components/Login.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="login_container">
+        <!-- 登录盒子  -->
+        <div class="login_box">
+            <!-- 头像 -->
+            <div class="avatar_box">
+                <img src="../assets/logo.png" alt="" />
+            </div>
+            <!-- 登录框 -->
+            <el-form v-model="loginForm" class="login_form" label-width="0px">
+                <!-- 用户名 -->
+                <el-form-item prop="username">
+                    <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+                </el-form-item>
+                <!-- 密码 -->
+                <el-form-item prop="password">
+                    <el-input v-model="loginForm.password" type="password" prefix-icon="iconfont icon-3702mima"></el-input>
+                </el-form-item>
+                <!-- 按钮 -->
+                <el-form-item class="btns">
+                    <el-button type="primary">登录</el-button>
+                    <el-button type="info">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            //数据绑定
+            loginForm: {
+                username: 'admin',
+                password: '123456'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+    background-color: #2b5b6b;
+    height: 100%;
+}
+.login_box {
+    width: 450px;
+    height: 300px;
+    background: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+}
+.avatar_box {
+    height: 130px;
+    width: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #ddd;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+    }
+}
+.login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.btns {
+    display: flex;
+    justify-content: flex-end;
+}
+</style>
