aae315b46aceb4ee32d6d49c8213b6dcb965e0c0
Yuki <490186360@qq.com>
2021-01-29 11:32:12 +0800

    退出功能和页面主体

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,6 +4,7 @@ import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
+import { Container, Header, Aside, Main } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -11,3 +12,7 @@ Vue.use(FormItem)
 Vue.use(Input)
 // 代表挂载属性和方法
 Vue.prototype.$message = Message
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
