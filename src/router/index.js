bbcbedec492c1a03305d0afbb3cc4d8d8939c977
Yuki <490186360@qq.com>
2021-01-29 16:22:42 +0800

    绘制用户列表组件路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -19,7 +20,10 @@ const routes = [
         path: '/home',
         component: Home,
         redirect: '/welcome',
-        children: [{ path: '/Welcome', component: Welcome }]
+        children: [
+            { path: '/Welcome', component: Welcome },
+            { path: '/users', component: Users }
+        ]
     }
 ]
 
