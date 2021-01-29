276e1d052466478ed6c7c2c470d3e09b233638bf
Yuki <490186360@qq.com>
2021-01-29 15:58:28 +0800

    设置Welcome组件路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
 
 Vue.use(VueRouter)
 
@@ -16,7 +17,9 @@ const routes = [
     },
     {
         path: '/home',
-        component: Home
+        component: Home,
+        redirect: '/welcome',
+        children: [{ path: '/Welcome', component: Welcome }]
     }
 ]
 
