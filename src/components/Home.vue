a396131306b13a1b8ec827cf4bd118cf9344be2e
Yuki <490186360@qq.com>
2021-01-29 11:57:21 +0800

    页面初步侧边栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,40 @@
 <template>
     <el-container class="home-container">
         <!-- 头部区域 -->
-        <el-header>Header <el-button type="info" @click="logout">退出</el-button></el-header>
+        <el-header>
+            <div>
+                <img src="../assets/heima.png" alt />
+                <span>电商后台管理系统</span>
+            </div>
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
         <!-- 页面主体 -->
         <el-container>
             <!-- 侧边栏 -->
-            <el-aside width="200px">Aside</el-aside>
+            <el-aside width="200px">
+                <!-- 侧边栏菜单区域 -->
+                <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                    <!-- 一级菜单的模板区域 -->
+                    <el-submenu index="1">
+                        <!-- 一级菜单的模板区域 -->
+                        <template slot="title">
+                            <!-- 图标 -->
+                            <i class="el-icon-location"></i>
+                            <!-- 文本 -->
+                            <span>导航一</span>
+                        </template>
+                        <!-- 二级菜单 -->
+                        <el-submenu index="1-4">
+                            <template slot="title">
+                                <!-- 图标 -->
+                                <i class="el-icon-location"></i>
+                                <!-- 文本 -->
+                                <span>导航一</span>
+                            </template>
+                        </el-submenu>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
             <!-- 右侧主体 -->
             <el-main>Main</el-main>
         </el-container>
@@ -28,7 +57,20 @@ export default {
     height: 100%;
 }
 .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     background-color: #373d41;
+    padding-left: 0;
+    color: #fff;
+    font-size: 20px;
+    div {
+        display: flex;
+        align-items: center;
+        span {
+            margin-left: 15px;
+        }
+    }
 }
 .el-aside {
     background-color: #333744;
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,7 +4,7 @@ import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
-import { Container, Header, Aside, Main } from 'element-ui'
+import { Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -16,3 +16,8 @@ Vue.use(Container)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
+
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItemGroup)
+Vue.use(MenuItem)
