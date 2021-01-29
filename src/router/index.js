e98c4ef6a35985934d9a6870fb15223574cafc9b
Yuki <490186360@qq.com>
2021-01-29 10:56:31 +0800

    设置登陆头

--- src/router/index.js
+++ src/router/index.js
@@ -23,5 +23,13 @@ const routes = [
 const router = new VueRouter({
     routes
 })
-
+router.beforeEach((to, from, next) => {
+    // to 将要访问的路径
+    // from 代表从哪个路径跳转而来
+    // next 是一个函数 表示放行
+    if (to.path === '/login') return next()
+    // 获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+})
 export default router
