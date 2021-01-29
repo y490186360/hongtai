6dea3de1244d2b1d3c88ff1a0da4af3405db32e4
Yuki <490186360@qq.com>
2021-01-29 16:40:27 +0800

    default-active激活左侧菜单

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,7 +14,7 @@
             <el-aside :width="isCollapse ? '64px' : '200px'">
                 <div class="toggle-button" @click="toggleCollapse">|||</div>
                 <!-- 侧边栏菜单区域 -->
-                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router>
+                <el-menu background-color="#333744" text-color="#fff" active-text-color="#409EFF" unique-opened :collapse="isCollapse" :collapse-transition="false" router :default-active="activePath">
                     <!-- 一级菜单 -->
                     <el-submenu :index="item.id + ''" v-for="item in menulist" :key="item.id">
                         <!-- 一级菜单的模板区域 -->
@@ -25,7 +25,7 @@
                             <span>{{ item.authName }}</span>
                         </template>
                         <!-- 二级菜单 -->
-                        <el-menu-item :index="'/' + subItem.path + ''" v-for="subItem in item.children" :key="subItem.id">
+                        <el-menu-item :index="'/' + subItem.path + ''" v-for="subItem in item.children" :key="subItem.id" @click="saveNavState('/' + subItem.path + '')">
                             <template slot="title">
                                 <!-- 图标 -->
                                 <i class="el-icon-menu"></i>
@@ -57,11 +57,14 @@ export default {
                 '102': 'iconfont icon-danju',
                 '145': 'iconfont icon-baobiao'
             },
-            isCollapse: false
+            isCollapse: false,
+            // 被激活的链接地址
+            activePath: ''
         }
     },
     created() {
         this.getMenuList()
+        this.activePath = window.sessionStorage.getItem('activePath')
     },
     methods: {
         logout() {
@@ -76,6 +79,10 @@ export default {
         },
         toggleCollapse() {
             this.isCollapse = !this.isCollapse
+        },
+        saveNavState(activePath) {
+            window.sessionStorage.setItem('activePath', activePath)
+            this.activePath = activePath
         }
     }
 }
