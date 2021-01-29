2f590acd4097ec51e6c138b5ed3b1dc020015b30
Yuki <490186360@qq.com>
2021-01-29 19:05:17 +0800

    渲染用户列表界面

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,11 +1,74 @@
 <template>
     <div>
-        <h3>用户列表组件</h3>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片试图 -->
+        <el-card>
+            <el-row :gutter="20">
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加用户</el-button>
+                </el-col>
+            </el-row>
+
+            <!-- 用户列表区 -->
+            <template>
+                <el-table :data="userlist" border stripe>
+                    <el-table-column type="index" label="#"></el-table-column>
+                    <el-table-column label="姓名" prop="username"></el-table-column>
+                    <el-table-column label="邮箱" prop="email"></el-table-column>
+                    <el-table-column label="电话" prop="mobile"></el-table-column>
+                    <el-table-column label="角色" prop="role_name"></el-table-column>
+                    <el-table-column label="状态">
+                        <template slot-scope="scope">
+                            <el-switch v-model="scope.row.mg_state"> </el-switch>
+                        </template>
+                    </el-table-column>
+                    <el-table-column label="操作"></el-table-column>
+                </el-table>
+            </template>
+        </el-card>
     </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            queryInfo: {
+                query: '',
+                pagenum: 1,
+                pagesize: 2
+            },
+            userlist: [],
+            total: 0
+        }
+    },
+    created() {
+        this.getUserList()
+    },
+    methods: {
+        async getUserList() {
+            const { data: res } = await this.$http.get('users', {
+                params: this.queryInfo
+            })
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取用户列表失败！')
+            }
+            this.userlist = res.data.users
+            this.total = res.data.total
+            console.log(res)
+        }
+    }
+}
 </script>
 
 <style lang="less" scope></style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -4,7 +4,7 @@ import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
-import { Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem } from 'element-ui'
+import { Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, col, table, TableColumn, Switch } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -19,5 +19,16 @@ Vue.use(Main)
 
 Vue.use(Menu)
 Vue.use(Submenu)
-Vue.use(MenuItemGroup)
+
 Vue.use(MenuItem)
+
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(col)
+
+Vue.use(table)
+Vue.use(TableColumn)
+
+Vue.use(Switch)
