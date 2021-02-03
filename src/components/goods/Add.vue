bd6c2dc322a9913bce7a2a6063a701d0598241a6
Yuki <490186360@qq.com>
2021-02-03 11:15:29 +0800

    增删查

---
+++ src/components/goods/Add.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片试图 -->
+        <el-card>
+            <!-- 提示区域 -->
+            <el-alert title="消息提示的文案" type="info" center show-icon> </el-alert>
+            <!-- 步骤条区 -->
+            <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+                <el-step title="基本信息"></el-step>
+                <el-step title="商品参数"></el-step>
+                <el-step title="商品属性"></el-step>
+                <el-step title="商品图片"></el-step>
+                <el-step title="商品内容"></el-step>
+                <el-step title="完成"></el-step>
+            </el-steps>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeIndex: 0
+        }
+    },
+    created() {},
+    methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,7 @@ import { Input } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
 import { Container, Header, Aside, Main, Menu, Submenu, MenuItem, Breadcrumb, BreadcrumbItem, Card, Row, col, table, TableColumn, Switch, Tooltip, Pagination, Dialog } from 'element-ui'
-import { MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane } from 'element-ui'
+import { MessageBox, Tag, Tree, Select, Option, Cascader, Alert, Tabs, TabPane, Step, Steps } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -57,3 +57,6 @@ Vue.use(Alert)
 
 Vue.use(Tabs)
 Vue.use(TabPane)
+// 步骤条
+Vue.use(Steps)
+Vue.use(Step)
