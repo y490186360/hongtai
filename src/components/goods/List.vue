53a764f77745bcd7c15e143b5cf58665a951b229
Yuki <490186360@qq.com>
2021-02-03 09:43:52 +0800

    获取商品列表数据

---
+++ src/components/goods/List.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <!-- 面包屑导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+            <el-breadcrumb-item>商品列表</el-breadcrumb-item>
+        </el-breadcrumb>
+        <!-- 卡片试图 -->
+        <el-card>
+            <el-row :gutter="20">
+                <el-col :span="8">
+                    <el-input placeholder="请输入内容">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加商品</el-button>
+                </el-col>
+            </el-row>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            // 查询参数对象
+            queryInfo: {
+                query: '',
+                pagenum: 1,
+                pagesize: 10
+            },
+            // 商品列表
+            goodsList: [],
+            // 总数据条数
+            total: 0
+        }
+    },
+    created() {
+        this.getDoodsList()
+    },
+    methods: {
+        // 根据分页获取对应的商品列表
+        async getDoodsList() {
+            const { data: res } = await this.$http.get('goods', {
+                params: this.queryInfo
+            })
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取商品列表失败！')
+            }
+            this.$message.success('获取商品列表成功！')
+            console.log(res.data)
+            this.goodsList = res.data.goods
+            this.total = res.data.total
+        }
+    }
+}
+</script>
